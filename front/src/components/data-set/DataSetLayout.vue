15e45e403425217dea6361027b7122e4cb8f031f
kido-k <kotaro.k13@gmail.com>
2021-05-30 17:49:17 +0900

    画像取得APIとレイアウトを変更

---
+++ front/src/components/data-set/DataSetLayout.vue
@@ -0,0 +1,210 @@
+<template>
+  <section class="image">
+    <div class="image__search">
+      <div v-if="resultStatus" class="image__status">
+        <v-simple-table>
+          <template #default>
+            <thead>
+              <tr>
+                <th class="text-left">検索ワード</th>
+                <th class="text-left">ステータス</th>
+                <th class="text-left">最大取得件数</th>
+                <th class="text-left">取得件数</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(key, index) in Object.keys(resultStatus)"
+                :key="index"
+              >
+                <td>{{ key }}</td>
+                <td>{{ resultStatus[key].getImageStatus }}</td>
+                <td>{{ resultStatus[key].maxGetNumber }}</td>
+                <td>{{ resultStatus[key].getImageNumber }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </div>
+      <v-layout align-center>
+        <v-text-field
+          v-model="searchName"
+          label="検索ワード"
+          outlined
+          type="text"
+          hide-details
+        />
+        <v-text-field
+          v-model="maxGetNumber"
+          label="最大取得件数"
+          type="number"
+          class="image__max-get-number"
+          hide-details
+        />
+        <v-btn
+          class="image__search__button text-button"
+          outlined
+          large
+          :disabled="!searchName"
+          @click="getImage"
+        >
+          GET
+        </v-btn>
+      </v-layout>
+    </div>
+    <div class="result">
+      <v-layout align-center class="result__select">
+        <v-select
+          v-model="selectResult"
+          :items="searchList"
+          class="result__select__selector"
+        />
+        <v-btn
+          class="result__update__button text-button"
+          large
+          outlined
+          @click="updateImageList"
+        >
+          UPDATE
+        </v-btn>
+      </v-layout>
+      <v-row v-if="selectResult" class="result__view__layout">
+        <v-col
+          v-for="(image, index) in selectImageData"
+          :key="index"
+          class="d-flex child-flex"
+          cols="4"
+        >
+          <v-card class="result__view__card">
+            <v-img class="result__view__content" :src="image.path" />
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchName: '',
+      maxGetNumber: 50,
+      imageData: {},
+      searchList: [],
+      selectResult: null,
+      resultRef: null,
+    }
+  },
+  computed: {
+    selectImageData() {
+      if (!this.selectResult) return []
+      const selectImage = this.imageData[this.selectResult]
+      if (!selectImage) return []
+
+      const selectImageList = []
+      Object.keys(selectImage).forEach((key) => {
+        selectImageList.push(selectImage[key])
+      })
+      return selectImageList
+    },
+    resultStatus() {
+      if (!this.resultRef) return
+      let status = null
+      this.resultRef.on('value', (snapshot) => {
+        status = snapshot.val()
+      })
+      return status
+    },
+  },
+  mounted() {
+    this.updateImageList()
+    this.resultRef = this.$firebase.database().ref('results/images')
+  },
+  methods: {
+    getImage() {
+      this.resultRef.child(this.searchName).set({
+        getImageStatus: 'start',
+        maxGetNumber: this.maxGetNumber || 50,
+        getImageNumber: 0,
+      })
+      this.deleteImageList()
+      const getImageUrl =
+        'http://localhost:5000/get_images' +
+          '?search_name=' +
+          this.searchName +
+          '&max_get_number=' +
+          this.maxGetNumber || 50
+      this.$getApi(getImageUrl)
+      this.searchName = ''
+      this.maxGetNumber = 50
+    },
+    updateImageList() {
+      this.$firebase
+        .database()
+        .ref('images')
+        .once('value', (snapshot) => {
+          this.imageData = snapshot.val()
+          this.searchList = Object.keys(this.imageData)
+        })
+    },
+    deleteImageList() {
+      this.$firebase.database().ref('images').child(this.searchName).set({})
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.image {
+  &__title {
+    font-size: 48px;
+    margin: 0 0 20px 0;
+  }
+  &__status {
+    margin: 10px 0;
+  }
+  &__max-get-number {
+    margin: 0 10px 0 30px !important;
+    max-width: 100px !important;
+  }
+  &__search {
+    padding: 20px;
+    margin: 0 0 20px 0;
+    border-radius: 10px;
+    border: 2px solid;
+    &__button {
+      margin: 0 0 0 20px;
+    }
+  }
+}
+
+.result {
+  padding: 20px;
+  margin: 0 0 20px 0;
+  border-radius: 10px;
+  border: 2px solid;
+  &__select {
+    max-width: 500px;
+  }
+  &__update {
+    &__button {
+      margin: 0 0 0 20px;
+    }
+  }
+  &__view {
+    &__layout {
+      padding: 20px;
+    }
+    &__card {
+      margin: auto;
+      padding: 20px;
+    }
+    &__content {
+      width: 200px;
+      margin: 10px;
+      text-align: center;
+    }
+  }
+}
+</style>
--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -1,194 +1,40 @@
 <template>
-  <section class="image">
-    <h1 class="image__title">学習用画像</h1>
-    <div class="image__search">
-      <p>新しく画像を取得する</p>
-      <div v-if="resultStatus" class="image__status">
-        <v-simple-table>
-          <template #default>
-            <thead>
-              <tr>
-                <th class="text-left">検索ワード</th>
-                <th class="text-left">ステータス</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(key, index) in Object.keys(resultStatus)"
-                :key="index"
-              >
-                <td>{{ key }}</td>
-                <td>{{ resultStatus[key].getImage }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </div>
-      <v-layout align-center>
-        <v-text-field
-          v-model="searchName"
-          label="検索ワード"
-          outlined
-          type="text"
-          hide-details
-        />
-        <v-text-field
-          v-model="maxGetNumber"
-          label="最大取得件数"
-          type="number"
-          class="image__max-get-number"
-          hide-details
-        />
-        <v-btn
-          class="image__search__button"
-          large
-          color="primary"
-          :disabled="!searchName"
-          @click="getImage"
-        >
-          取得
-        </v-btn>
-      </v-layout>
+  <section class="wrap">
+    <div class="text-center justify-center py-6">
+      <h1 class="font-weight-bold display-3 basil--text">GaKuShu</h1>
     </div>
-    <div class="image__result">
-      <v-layout max-width="400px">
-        <v-select
-          v-model="selectResult"
-          :items="searchList"
-          class="image__result__select"
-        />
-        <v-btn
-          class="image__search__button"
-          large
-          color="primary"
-          @click="updateImageList"
-        >
-          更新
-        </v-btn>
-      </v-layout>
-      <v-row v-if="selectResult" class="image__result__layout">
-        <v-col
-          v-for="(image, index) in selectImageData"
-          :key="index"
-          class="d-flex child-flex"
-          cols="4"
-        >
-          <v-card class="image__result__card">
-            <v-img class="image__result__content" :src="image.path" />
-          </v-card>
-        </v-col>
-      </v-row>
-    </div>
+    <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+      <v-tab v-for="item in tabItems" :key="item">
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+    <v-card v-if="tab === 0" class="mt-10">
+      <data-set-layout />
+    </v-card>
   </section>
 </template>
 
 <script>
+import DataSetLayout from '@/components/data-set/DataSetLayout.vue'
+
 export default {
-  components: {},
+  components: {
+    DataSetLayout,
+  },
   data() {
     return {
-      searchName: '',
-      maxGetNumber: 50,
-      imageData: {},
-      searchList: [],
-      selectResult: null,
-      resultRef: null,
+      tab: 0,
+      tabItems: ['DataSet', 'Learning', 'Judge'],
     }
   },
-  computed: {
-    selectImageData() {
-      if (!this.selectResult) return []
-      const selectImage = this.imageData[this.selectResult]
-      if (!selectImage) return []
-
-      const selectImageList = []
-      Object.keys(selectImage).forEach((key) => {
-        selectImageList.push(selectImage[key])
-      })
-      return selectImageList
-    },
-    resultStatus() {
-      if (!this.resultRef) return
-      let status = null
-      this.resultRef.on('value', (snapshot) => {
-        status = snapshot.val()
-      })
-      return status
-    },
-  },
-  mounted() {
-    this.resultRef = this.$firebase.database().ref('results/images')
-  },
-  methods: {
-    getImage() {
-      this.resultRef.child(this.searchName).set({
-        getImage: 'start',
-      })
-      const getImageUrl =
-        'http://localhost:5000/get_images' +
-          '?search_name=' +
-          this.searchName +
-          '&max_get_number=' +
-          this.maxGetNumber || 50
-      this.$getApi(getImageUrl)
-      this.searchName = ''
-      this.maxGetNumber = 50
-    },
-    updateImageList() {
-      this.$firebase
-        .database()
-        .ref('images')
-        .once('value', (snapshot) => {
-          this.imageData = snapshot.val()
-          this.searchList = Object.keys(this.imageData)
-        })
-    },
-  },
+  computed: {},
+  methods: {},
 }
 </script>
 
 <style lang="scss">
-.image {
-  &__title {
-    font-size: 48px;
-    margin: 0 0 20px 0;
-  }
-  &__status {
-    margin: 10px 0;
-  }
-  &__max-get-number {
-    margin: 0 10px 0 30px !important;
-    max-width: 100px !important;
-  }
-  &__search {
-    padding: 20px;
-    margin: 0 0 20px 0;
-    border-radius: 10px;
-    border: 2px solid #2196f3;
-    &__button {
-      margin: 0 0 0 20px;
-    }
-  }
-
-  &__result {
-    padding: 20px;
-    border-radius: 10px;
-    border: 2px solid #2196f3;
-    &__select {
-      width: 200px;
-    }
-    &__layout {
-      padding: 20px;
-    }
-    &__card {
-      margin: auto;
-      padding: 20px;
-    }
-    &__content {
-      width: 200px;
-      margin: 10px;
-      text-align: center;
-    }
-  }
+.wrap {
+  max-width: 900px;
+  margin: auto;
 }
 </style>
