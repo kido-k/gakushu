6908d9cec26b8510a7a1bd8f9dac639e1ab07908
kido-k <kotaro.k13@gmail.com>
2021-06-02 23:02:23 +0900

    画像のトレーニングデータをnumpyファイルで出力

--- server/src/app.py
+++ server/src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import flickr_download
+import gen_data
 
 app = Flask(__name__)
 
@@ -12,6 +13,11 @@ def get_images():
     search_name = request.args.get('search_name')
     max_get_number = request.args.get('max_get_number')
     flickr_download.get_images(search_name, max_get_number)
+    return "OK"
+
+@app.route('/learning')
+def learning():
+    gen_data.create_numpy_data('dog-cat-bird', ['dog', 'cat', 'bird'])
     return "OK"
 
 if __name__ == '__main__':
---
+++ server/src/gen_data.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import os, glob
+import numpy as np
+from sklearn import model_selection
+
+IMAGE_SIZE = 50
+
+def create_numpy_data(category, classes):
+    X = []
+    Y = []
+    for index, classlabel in enumerate(classes):
+        photos_dir = './src/images/' + classlabel 
+        files = glob.glob(photos_dir + "/*.jpg")
+        for i, file in enumerate(files):
+            if i >= 200: break
+            image = Image.open(file)
+            image = image.convert('RGB')
+            image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
+            data = np.asarray(image)
+            X.append(data)
+            Y.append(index)
+
+    X = np.array(X)
+    Y = np.array(Y)
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, Y)
+    xy = (X_train, X_test, y_train, y_test )
+    np.save('./src/npy/' + category + '.npy', xy)
+
+
