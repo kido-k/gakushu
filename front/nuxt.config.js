3a17981875efeedc73382872475adb017771cc19
kido-k <kotaro.k13@gmail.com>
2021-06-05 20:44:08 +0900

    titleを変更

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   srcDir: 'src/',
   head: {
-    title: 'gakushu',
+    title: 'GaKuShu',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
